eb63ec90344a51025805457b0c0abe2dcea67fd4
Ben Tau <62897867+bentau19@users.noreply.github.com>
2025-01-19 00:06:20 +0200

    most opt for now

--- max-clique.c
+++ max-clique.c
@@ -5,8 +5,15 @@
 #define MAX_VERTICES 100
 
 void printSolution(signed char vertices[], signed char size) {
-    for (signed char i = 0; i < size; i++) {
+    signed char gap =size%4;
+    for (signed char i = 0; i < size%4; i++){
         printf("%d ", vertices[i]);
+    }
+    for (signed char i = gap; i < size; i+=4) {
+        printf("%d ", vertices[i]);
+        printf("%d ", vertices[i+1]);
+        printf("%d ", vertices[i+2]);
+        printf("%d ", vertices[i+3]);
     }
     printf("\n");
 }
@@ -25,40 +32,32 @@ bool isClique(bool graph[MAX_VERTICES][MAX_VERTICES], signed char clique[], sign
 }
 
 
-void generateCombinations( bool graph[MAX_VERTICES][MAX_VERTICES], 
+bool generateCombinations( bool graph[MAX_VERTICES][MAX_VERTICES], 
 signed char n, signed char *clique, signed char k, signed char start,
- signed char currentSize, signed char *maxSize, signed char *maxClique,bool *flag,signed char deg[]) {
-    // printf("testing {");
-    // for(int i = 0; i < currentSize; i++){
-    //     printf("%d ",clique[i]);
-    // }
-    // printf("}\n");
-     if(*flag)return;
+ signed char currentSize, signed char *maxSize, signed char *maxClique,signed char deg[]) {
     if (currentSize == k) {
         if (isClique(graph, clique, k)) {
-            // printf("find!!\n");
-            *flag=true;
             if (k > *maxSize) {
                 *maxSize = k;
                 for (int i = 0; i < k; i++) {
                     maxClique[i] = clique[i];
                 }
             }
+            return true;
         }
-        return;
+        return false;
     }
-    signed char t = k-1;
+    signed char t = k-2;
+    signed char t2 = currentSize+n;
     for (int i = start; i < n; i++) {
-        if(k>(n-i)+currentSize)return;
-        // printf("deg %d is %d while k is %d \n",i,deg[i],k);
-        if(deg[i]>=t){
-        // printf("deg %d is %d while k is %d \n",i,deg[i],k);
+        if(k>t2-i)return false;
+        if(deg[i]>t){
         clique[currentSize] = i;
-        // printf("Calling\n");
-        generateCombinations(graph, n, clique, k, i + 1, currentSize + 1, maxSize, maxClique, flag,deg);
-    //    if(*flag)return;
+        if(generateCombinations(graph, n, clique, k, i + 1, currentSize + 1, maxSize, maxClique,deg))
+        return true;
         }
       }
+      return false;
 }
 
 
@@ -78,21 +77,18 @@ void initGraph(int graph[MAX_VERTICES][MAX_VERTICES],bool optGraph[MAX_VERTICES]
 }
 
 void findMaxClique(int graph[MAX_VERTICES][MAX_VERTICES], int n) {
-    signed char clique[n];
-    signed char maxClique[n];
+    signed char size = n * sizeof(signed char);
+    signed char *clique = (signed char *)malloc(size);
+    signed char *maxClique = (signed char *)malloc(size);
+    signed char *deg = (signed char *)calloc(n, sizeof(signed char));
     signed char maxSize = 0;
-    signed char deg[n];
-    for (int i =0;i<n;i++)deg[i]=0;
     bool optGraph [MAX_VERTICES][MAX_VERTICES];
     initGraph(graph,optGraph,n,deg);
 
 int left = 0, right = n;
  while (left <= right) {
-        bool flag=false;
         int k = left + ((right - left) >>1);
-        // printf("k is %d now!\n",k);
-        generateCombinations(optGraph, n, clique, k, 0, 0, &maxSize, maxClique,&flag,deg);
-     
+        bool flag = generateCombinations(optGraph, n, clique, k, 0, 0, &maxSize, maxClique,deg);
         if (flag) {
             left = k + 1; // Move the left pointer to find the last true
         } else {
@@ -103,5 +99,7 @@ int left = 0, right = n;
     printf("Clique Members: ");
     printSolution(maxClique, maxSize);
     printf("Size: %d\n", maxSize);
-
+free(clique);
+free(maxClique);
+free(deg);
 }
